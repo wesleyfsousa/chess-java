46e965ea5de2cdbffbadc99ff75b4dfe0217037c
Wesley Fernandes Sousa <wesley.fernandesousa@gmail.com>
2021-02-18 15:19:31 -0300

    Inicio da implementação do Board e Piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,11 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
-public class App {
+public class App
+{
     public static void main(String[] args)
     {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(3, 5);
     }
 }
