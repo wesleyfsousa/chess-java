43b693d3b9cfd47f5abcf48809212875c004103c
Wesley Fernandes Sousa <wesley.fernandesousa@gmail.com>
2021-02-18 16:17:50 -0300

    criação da UI do board chess

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,14 @@
 package application;
 
-import boardgame.Board;
+
+import chess.ChessMatch;
 
 public class App
 {
     public static void main(String[] args)
     {
-        Board board = new Board(3, 5);
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
     }
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -13,19 +13,33 @@ public class Board
         pieces = new Piece[row][column]; 
     }
 
-    public int getRow() {
+    public int getRow()
+    {
         return row;
     }
 
-    public void setRow(int row) {
+    public void setRow(int row)
+    {
         this.row = row;
     }
 
-    public int getColumn() {
+    public int getColumn()
+    {
         return column;
     }
 
-    public void setColumn(int column) {
+    public void setColumn(int column)
+    {
         this.column = column;
+    }
+
+    public Piece piece(int row,int column)
+    {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position)
+    {
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
